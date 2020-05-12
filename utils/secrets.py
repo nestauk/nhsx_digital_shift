ffa36ab9fe39b0222bfc814eb9546b7de10d9fa7
Joel Klinger <joel.alexander.klinger@gmail.com>
2020-05-12 09:19:46 +0100

    strips non-alphanums from key

--- utils/secrets.py
+++ utils/secrets.py
@@ -8,4 +8,5 @@ def news_api_key():
     file_dir = pathlib.Path(__file__).parent.absolute()
     base_dir = os.path.dirname(file_dir)
     with open(os.path.join(base_dir, 'secrets/news_api_key')) as f:
-        return f.read()
+        value = f.read()
+        return "".join(value.split())
